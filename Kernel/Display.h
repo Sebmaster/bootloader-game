fdd5aa586133730713992e9c3ab6884e4413e785
Sebastian Mayr <sebmaster16@gmail.com>
2013-04-10 19:17:36 +0200

    Removes unnecessary kernel files.

--- Kernel/Display.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Display {
-public:
-	static void initText();
-	static void print(const char* str);
-	static void setChar(char x, char y, char chr, char col);
-	static void disableCursor();
-};
--- Kernel/stage1.cpp
+++ Kernel/stage1.cpp
@@ -1,4 +1,10 @@
-#include "stdlib.h"
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL 0
+#else
+#define NULL ((void *)0)
+#endif
+#endif
 
 struct Node {
 	char x;
